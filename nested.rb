10c3ac0a6485fa91ba291f0424766592ec3b3373
Tejas Cousik <tcousik0@gmail.com>
2019-04-22 00:14:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,12 +16,9 @@ def hopper
         }
      }
   return programmer_hash[:grace_hopper]
-
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,9 +34,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-
 return programmer_hash[:alan_kay][:known_for]
-
 end
 
 def dennis_ritchies_language
@@ -58,6 +53,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+return programmer_hash[:dennis_ritchie][:languages.to_s]
 end
 
 def adding_matz
